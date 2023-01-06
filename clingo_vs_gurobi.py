899351b9672312471657a2d841e5f9d6b2c2b370
Pavel Medina <37118347+aamedinaj@users.noreply.github.com>
2023-01-06 01:30:59 -0300

    setting thing up with experiment class

--- clingo_vs_gurobi.py
+++ clingo_vs_gurobi.py
@@ -9,7 +9,7 @@ from automatas_approach.MILP import milp
 
 
 def read_json(path):
-    with open(path, 'r') as json_file:
+    with open(path, "r") as json_file:
         return json.load(json_file)
 
 
@@ -24,11 +24,11 @@ class Graph:
         self.objects = read_json(objects_path)
         self.negative_traces_case = negative_traces_case
 
-    def generate_trace(self):
-        pass
+    def generate_traces(self, n_steps: int):
+        ...
 
-    def trace_to_lp(self, trace):
-        pass
+    def traces_to_lp(self, traces):
+        ...
 
 
 class ClingoExperiment:
@@ -37,8 +37,9 @@ class ClingoExperiment:
         self.max_dag_nodes = max_dag_nodes
         self.max_dag_tries = max_dag_tries
 
-    def run_experiment(self):
-        pass
+    def run(self, traces):
+        # invoke the clingo solver from ./main.py
+        ...
 
 
 class GurobiExperiment:
@@ -50,8 +51,9 @@ class GurobiExperiment:
         g = God(all_traces)
         self.arbol = g.give_me_the_plant()
 
-    def run_experiment(self):
-        pass
+    def run(self, traces):
+        # invoke the gurobi solver from milp
+        ...
 
 
 class Experiment:
@@ -71,21 +73,44 @@ class Experiment:
 
         self.graph = Graph(conn_path, objs_path, negative_traces_case)
         self.clingo_exp = ClingoExperiment(max_dag_nodes, max_dag_tries)
-        # self.gurobi_exp = GurobiExperiment(max_automata_states)
+        self.gurobi_exp = GurobiExperiment(max_automata_states)
+
+    def run(self):
+        cli_times = []
+        gur_times = []
+        steps_range = range(self.min_trace_steps, self.max_trace_steps + 1)
+        for n_steps in steps_range:
+            traces_cli = self.graph.generate_traces(n_steps)  # TODO
+            traces_gur = self.graph.traces_to_lp(traces_cli)
+
+            time_cli_start = perf_counter()
+            self.clingo_exp.run(traces_cli)  # TODO
+            time_cli_end = perf_counter()
+
+            time_gur_start = perf_counter()
+            self.gurobi_exp.run(traces_gur)  # TODO
+            time_gur_end = perf_counter()
+
+            cli_times.append(time_cli_start - time_cli_end)
+            gur_times.append(time_gur_start - time_gur_end)
+
+        data = {"clng": cli_times, "grb": gur_times}
+        return pd.DataFrame(data, index=steps_range)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
-    logging.debug('Initial log')
-    
-    
+    logging.debug("Initial log")
+
     exp = Experiment(
-        conn_path= './traces_ch/B6ByNegPMKs_connectivity.json',
-        objs_path= './traces_ch/B6ByNegPMKs_objects.json',
-        min_trace_steps= 3,
-        max_trace_steps= 10,
-        negative_traces_case= 1,
-        max_dag_nodes= 10,
-        max_dag_tries= 5000,
-        max_automata_states= 10,
+        conn_path="./traces_ch/B6ByNegPMKs_connectivity.json",
+        objs_path="./traces_ch/B6ByNegPMKs_objects.json",
+        min_trace_steps=3,
+        max_trace_steps=10,
+        negative_traces_case=1,
+        max_dag_nodes=10,
+        max_dag_tries=5000,
+        max_automata_states=10,
     )
+    results = exp.run()
+    print(results)
